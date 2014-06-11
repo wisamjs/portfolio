0c9e678644955994b87b10f1341580d6614816ac
wzaghal <wisamzaghal@gmail.com>
2014-06-10 22:22:24 -0400

    add icons

--- app/assets/javascripts/Home.js
+++ app/assets/javascripts/Home.js
@@ -1,4 +1,18 @@
   $(document).ready(function(){
-    $("#nav").sticky({topSpacing:0, className:"sticky" });
-    //$("#sticker").sticky({ topSpacing: 0, center:true, className:"hey" });
+  	$("#nav").sticky({topSpacing:0, className:"sticky" });
+  	var downArrow = $('.down-arrow');
+  	setInterval(function(){
+    	downArrow.toggleClass('pulse');
+    	console.log('test');
+    },700);
+
+
+    $(document).keyup(function(e) {
+    	console.log('you hit something: ' + e.which);
+
+    	if (e.which === 13){
+    		console.log('ENTER MUTHA FUCKA');
+    	}
+
+    });
   });
