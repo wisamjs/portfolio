5919e7bcbec215603e09dc0d549368adc50a6741
wzaghal <wisamzaghal@gmail.com>
2014-06-26 13:46:13 -0400

    fix broken navbar

--- app/assets/javascripts/Home.js
+++ app/assets/javascripts/Home.js
@@ -3,16 +3,5 @@
   	var downArrow = $('.down-arrow');
   	setInterval(function(){
     	downArrow.toggleClass('pulse');
-    	console.log('test');
     },700);
-
-
-    $(document).keyup(function(e) {
-    	console.log('you hit something: ' + e.which);
-
-    	if (e.which === 13){
-    		console.log('ENTER MUTHA FUCKA');
-    	}
-
-    });
   });
