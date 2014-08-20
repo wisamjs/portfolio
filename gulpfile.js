645418acb618e3392dbc135784a7e56d67a6848d
wzaghal <wisamzaghal@gmail.com>
2014-08-20 19:59:54 -0400

    add gulp tasks to minify code when deploying

--- gulpfile.js
+++ gulpfile.js
@@ -8,10 +8,15 @@ var gulp = require('gulp'),
 	stylish = require('jshint-stylish'),
 	livereload = require('gulp-livereload'),
 	connect = require('gulp-connect'),
+  minifyCSS = require('gulp-minify-css'),
+  minifyHTML = require('gulp-minify-html'),
+  imagemin = require('gulp-imagemin'),
+  pngcrush = require('imagemin-pngcrush'),
 
 	//variables to reference all javascript files
 	jsFiles = ['scripts/*.js'],
-	scssFiles = ['styles/main.scss'];
+	scssFiles = ['styles/main.scss'],
+  cssFiles = ['styles/main.css','styles/normalize.css'];
 
 
 // Precompile Sass
@@ -58,7 +63,30 @@ gulp.task('watch', function() {
   gulp.watch(jsFiles, ['lint']);
 });
 
+gulp.task('minify-css', function() {
+  gulp.src(cssFiles)
+    .pipe(minifyCSS({keepBreaks:true}))
+    .pipe(gulp.dest('build/styles/'))
+});
 
+gulp.task('minify-html', function() {
+    var opts = {comments:true,spare:true};
+
+  gulp.src('index.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('build/'))
+});
+
+gulp.task('minify-img', function () {
+    return gulp.src('images/IMG_55493.jpg')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest('build/images'));
+});
 
 //task to run all
 gulp.task('default', ['devServer','sass','lint', 'watch']);
+gulp.task('build',['sass','minify-html','minify-css','minify-img']);
