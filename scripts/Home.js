c2338955ffeab35a5bdc650a8142cf9ddfffb00b
wzaghal <wisamzaghal@gmail.com>
2015-06-14 21:55:42 -0400

    modify twitter handle

--- scripts/Home.js
+++
@@ -1,7 +0,0 @@
-  $(document).ready(function(){
-  	$("#nav").sticky({topSpacing:0, className:"sticky" });
-  	var downArrow = $('.down-arrow');
-  	setInterval(function(){
-    	downArrow.toggleClass('pulse');
-    },700);
-  });
